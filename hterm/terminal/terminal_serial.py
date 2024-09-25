1d75a3a664bc931a371ab04eaf824e2ae9b8e50d
lbhzy <296384478z@gmail.com>
2024-09-25 23:42:28 +0800

    串口终端新增open close接口，完善连接逻辑

--- hterm/terminal/terminal_serial.py
+++ hterm/terminal/terminal_serial.py
@@ -13,25 +13,47 @@ class SerialTerm(Terminal):
     def __init__(self, port, baud):
         super().__init__()
 
+        self.port = port
+        self.baud = baud
+        self.connected = False
+
         self.timer = QTimer(self)
         self.timer.setSingleShot(True)
         self.timer.timeout.connect(self.timerCallback)
         
+    def open(self):
         try:
-            self.ser = serial.Serial(port, baud, timeout=0.01)
+            self.ser = serial.Serial(self.port, self.baud, timeout=0.01)
             self.timer.start(0)
+            self.connected = True
+            self.display(f"连接成功\n")
         except Exception as e:
-            self.display(f"Error: {e}")
+            self.connected = False
+            self.display(f"连接失败: {e}\n")
+
+    def close(self):
+        if self.connected:
+            self.ser.close()
               
     def sendData(self, data):
-        if self.ser.is_open:
+        if self.connected:
             self.ser.write(data.encode("utf-8"))
+        else:
+            self.open()
 
 
     def timerCallback(self):
-        if self.ser.in_waiting > 0:
-            data = self.ser.readline()
-            text = data.decode('utf-8')
+        try:
+            data = None
+            if self.ser.in_waiting:
+                data = self.ser.readline()
+        except Exception:
+            self.connected = False
+            self.display(f"连接断开\n")
+            return
+        
+        if data:
+            text = data.decode("utf-8", "replace")
             self.display(text)
             self.timer.start(0)
         else:
@@ -41,5 +63,7 @@ if __name__ == "__main__":
 
     app = QApplication()
     term = SerialTerm("COM3", 115200)
+    term.open()
+    term.close()
     term.show()
     app.exec()
