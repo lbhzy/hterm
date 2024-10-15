0386bdc142fe1608c86a9651ae2e129b4fd149fc
lbhzy <296384478z@gmail.com>
2024-10-16 00:32:37 +0800

    修改bat脚本

--- build.bat
+++ build.bat
@@ -1,12 +1,13 @@
 @echo off
 
 pyinstaller -y ^
--i images/icon.png ^
+-i ../images/icon.png ^
 -p hterm/terminal;hterm/ui ^
+--specpath build ^
+--collect-data winpty ^
 -n Hterm ^
 hterm/main.py
 
-del *.spec
 
 xcopy /y /q /S /E /I ".\hterm\images" ".\dist\hterm\images"
 xcopy /y /q /S /E /I ".\hterm\profiles" ".\dist\hterm\profiles"
