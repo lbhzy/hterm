8b96cb2c5ea9b461008ba94c1f19076d20bca583
lbhzy <296384478z@gmail.com>
2025-02-15 03:32:27 +0800

    使用uv管理项目

---
+++ build.py
@@ -0,0 +1,24 @@
+import shutil
+from PyInstaller.__main__ import run
+
+
+def build():
+    params = [
+        'hterm/main.py',
+        '-yw',
+        '-i=images/icon.png',
+        '-p=hterm/terminal;hterm/ui;hterm/common',
+        '--collect-data=winpty',
+        '--name=hterm'
+    ]
+    run(params)
+
+def copy_resources():
+    shutil.copytree("hterm\images", "dist\hterm\images")
+    shutil.copytree("hterm\schemes", "dist\hterm\schemes")
+
+
+if __name__ == "__main__":
+    build()
+    copy_resources()
+    
